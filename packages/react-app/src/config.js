5d60c78b31176107351edd9ecf2e0e4c85cc03bf
homero <homerogazze1015@gmail.com>
2022-10-08 18:19:49 -0300

    [FIX] Misspelled Alchemy URL

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -5,6 +5,6 @@ export const ROUTER_ADDRESS = "0xFAD0B7B122c5C7f4A48a19c38d61d40B85e0146C";
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "https://eth-mainnet.g.alchemy.com/v2/f1879Am78oS-ujOVr5Jy0N7gEyX4QQtq",
+    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/GUNsDs0FvXNCzey2QoLA8XHD70neYqfq",
   },
 };
