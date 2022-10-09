7448f9a51aa021fdad63c823f2cb22db52136ac3
homero <homerogazze1015@gmail.com>
2022-10-09 13:45:52 -0300

    [NEW] AmountOut Component Created

---
+++ packages/react-app/src/components/AmountOut.js
@@ -0,0 +1,66 @@
+import { formatUnits } from 'ethers/lib/utils';
+import React, { useState, useEffect,  useRef } from 'react'
+
+import { chevronDown } from '../assets'
+import styles from '../styles'
+import { useOnClickOutside,useAmountsOut } from '../utils/helpers'
+
+function AmountOut({ fromToken, toToken, amountIn, pairContract, currencyValue, onSelect, currencies }) {
+
+  const [showList, setShowList] = useState(false);
+  const [activeCurrency, setActiveCurrency] = useState("Select")
+  const ref = useRef();
+
+  useOnClickOutside(ref, () => setShowList(false));
+
+  const amountOut = useAmountsOut(pairContract, amountIn, fromToken, toToken) ?? 0;
+
+  useEffect(() => {
+    if (Object.keys(currencies).includes(currencyValue))
+      setActiveCurrency(currencies[currencyValue]);
+    else setActiveCurrency("Select");
+  }, [currencies, currencyValue]);
+
+  return (
+    <div className={styles.amountContainer}>
+      <input 
+        placeholder='0.0'
+        type="number"
+        value={formatUnits(amountOut)}
+        disabled
+        className={styles.amountInput}
+      />
+
+      <div className='relative' onClick={() => setShowList((prevState) => !prevState)}>
+        <button className={styles.currencyButton}>
+          {activeCurrency}
+          <img 
+            src={chevronDown}
+            alt="chevron down"
+            chassName={`w-4 h-4 object-contain ml-2 ${showList ? 'rotate-180' : 'rotate-0'}`}
+          />
+        </button>
+
+        {showList && (
+          <ul ref={ref} className={styles.currencyList}>
+            {Object.entries(currencies).map(([ token, tokenName ], i) => (
+                <li
+                  key={i}
+                  className={styles.currencyListItem}
+                  onClick={() => {
+                    if(typeof onSelect === "function") onSelect(token);
+                    setActiveCurrency(tokenName)
+                    setShowList(false);
+                  }}
+                >
+                  {tokenName}
+                </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default AmountOut
