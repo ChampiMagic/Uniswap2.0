512e651634656dd49091a4eb6ab523c4157e8bcc
homero <homerogazze1015@gmail.com>
2022-10-09 13:47:44 -0300

    [UPDATE] Updated app file with all connections created

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,7 +1,56 @@
 import React from "react"
+import { useEthers } from '@usedapp/core'
+
+import styles from './styles'
+
+import { uniswapLogo } from './assets'
+import { usePools } from './hooks'
+
+// Components
+import { Loader, Exchange, WalletButton } from './components'
 
 const App = () => {
-  return <p>Hello, Web3!</p>
+
+  const { account } = useEthers()
+
+  const [loading, pools] = usePools();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <header className={styles.header}>
+          <img 
+            src={uniswapLogo}
+            alt="uniswap logo"
+            className="w-16 h-16
+            object-contain"
+          />
+          <WalletButton />
+        </header>
+
+        <div className={styles.exchangeContainer}>
+          <h1 className={styles.headTitle}>Uniswap 2.0</h1>
+          <p className={styles.subTitle}>Exchange tokens un seconds</p>
+
+          <div className={styles.exchangeBoxWrapper}>
+            <div className={styles.exchangeBox}>
+              <div className="pink_gradient" />
+                <div className={styles.exchange}>
+                  {account ? (
+                    loading ? (
+                      <Loader title="Loading pools, please waite"/>
+                    ) : <Exchange pools={pools}/>
+                   ) : <Loader title="Please connect your wallet!"/>
+                  }
+                </div> 
+              <div className="blue_gradient" />
+    
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
